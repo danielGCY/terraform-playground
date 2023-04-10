83b1fa9fc00f1a0a8653912d2ce764e71b79ed50
Daniel Gan <99370769+daniel-gan@users.noreply.github.com>
2023-04-11 09:25:53 +1000

    Added more outputs to module

--- aws/modules/vpc/outputs.tf
+++ aws/modules/vpc/outputs.tf
@@ -2,10 +2,34 @@ output "vpc_id" {
   value = aws_vpc.main.id
 }
 
+output "vpc_cidr_block" {
+  value = aws_vpc.main.cidr_block
+}
+
+output "availability_zones" {
+  value = data.aws_availability_zones.default
+}
+
+output "internet_gateway_id" {
+  value = aws_internet_gateway.main.id
+}
+
 output "public_subnets_ids" {
   value = aws_subnet.public.*.id
 }
 
+output "public_subnets_route_table_id" {
+  value = aws_route_table.public.id
+}
+
+output "nat_gateway_id" {
+  value = aws_nat_gateway.main.id
+}
+
 output "private_subnets_ids" {
   value = aws_subnet.private.*.id
+}
+
+output "private_subnets_route_table_id" {
+  value = aws_route_table.private.id
 }
