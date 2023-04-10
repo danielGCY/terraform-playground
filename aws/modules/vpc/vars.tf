4b4f7885020550b9af5b8afc179956b9cdd3cda6
Daniel Gan <99370769+daniel-gan@users.noreply.github.com>
2023-04-11 09:35:36 +1000

    Updated input variables
    - Added `name_prefix` for tagging
    - Added types in variables

--- aws/modules/vpc/vars.tf
+++ aws/modules/vpc/vars.tf
@@ -1,8 +1,14 @@
+variable "name_prefix" {
+  type = string
+}
+
 variable "region" {
+  type    = string
   default = "ap-southeast-2"
 }
 
 variable "cidr_block" {
+  type        = string
   description = "CIDR block for VPC"
   default     = "10.0.0.0/16"
 }
@@ -12,11 +18,13 @@ variable "enable_dns_hostnames" {
 }
 
 variable "public_subnet_cidr_blocks" {
+  type        = list[string]
   description = "List of CIDR blocks for the public subnets"
   default     = ["10.0.0.0/24", "10.0.1.0/24"]
 }
 
 variable "private_subnet_cidr_blocks" {
+  type        = list[string]
   description = "List of CIDR blocks for the private subnets"
   default     = []
 }
